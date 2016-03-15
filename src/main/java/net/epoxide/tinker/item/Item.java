dc5dfc34bd130b2716422e970dd4f36d6fdd9588
Darkhax <darklime@live.ca>
2016-03-15 03:28:54 -0600

    Added the very extreme basics for Item.

---
+++ src/main/java/net/epoxide/tinker/item/Item.java
@@ -0,0 +1,8 @@
+package net.epoxide.tinker.item;
+
+import net.epoxide.tinker.util.NamedRegistry;
+
+public class Item {
+    
+    public static final NamedRegistry<Item> REGISTRY = new NamedRegistry<Item>();
+}
