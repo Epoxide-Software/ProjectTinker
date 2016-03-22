2507b85066e0399884e5d54834bdd79407949604
Darkhax <darklime@live.ca>
2016-03-22 02:19:40 -0600

    Re-added constructors.

--- src/main/java/net/epoxide/tinker/entity/living/EntityPlayer.java
+++ src/main/java/net/epoxide/tinker/entity/living/EntityPlayer.java
@@ -1,5 +1,37 @@
 package net.epoxide.tinker.entity.living;
 
-public class EntityPlayer extends EntityLiving {
+import net.darkhax.opennbt.tags.CompoundTag;
+import net.epoxide.tinker.world.TileMap;
 
+public class EntityPlayer extends EntityLiving {
+    
+    /**
+     * Constructs the entity with no internal logic. Allows for all of the logic to be handled
+     * by the entity.
+     */
+    public EntityPlayer() {
+    
+    }
+    
+    /**
+     * Constructs a new entity that is on a TileMap.
+     * 
+     * @param map The TileMap to spawn the entity on.
+     */
+    public EntityPlayer(TileMap map) {
+        
+        super(map);
+    }
+    
+    /**
+     * Constructs a new entity from a CompoundTag. Intended for loading entities onto a TileMap
+     * from the TileMap data.
+     * 
+     * @param map The TileMap to spawn the entity on.
+     * @param tag The CompoundTag to load data from.
+     */
+    public EntityPlayer(TileMap map, CompoundTag tag) {
+        
+        super(map, tag);
+    }
 }
