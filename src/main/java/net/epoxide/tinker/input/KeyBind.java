c677a97a0266f8c98a42f4e85427fc6382143b64
Darkhax <darklime@live.ca>
2016-03-26 01:00:46 -0600

    Formatting and imports for keybind stuff.

--- src/main/java/net/epoxide/tinker/input/KeyBind.java
+++ src/main/java/net/epoxide/tinker/input/KeyBind.java
@@ -1,47 +1,47 @@
 package net.epoxide.tinker.input;
 
 public class KeyBind {
-
+    
     private int key;
     public boolean isEvent;
     public boolean pressed;
-
-    public KeyBind (int key) {
-
+    
+    public KeyBind(int key) {
+        
         this(key, false);
     }
-
-    public KeyBind (int key, boolean isEvent) {
-
+    
+    public KeyBind(int key, boolean isEvent) {
+        
         this.key = key;
         this.isEvent = isEvent;
     }
-
+    
     public void setKey (int key) {
-
+        
         this.key = key;
     }
-
+    
     public int getKey () {
-
+        
         return this.key;
     }
-
+    
     public boolean isPressed () {
-
+        
         return this.pressed;
     }
-
+    
     public void setPressed (boolean b) {
-
+        
         this.pressed = b;
     }
-
+    
     public void onKeyPressed (float delta) {
-
+    
     }
-
+    
     public void onKeyReleased (float delta) {
-
+    
     }
 }
--- src/main/java/net/epoxide/tinker/input/KeyHandler.java
+++ src/main/java/net/epoxide/tinker/input/KeyHandler.java
@@ -1,28 +1,29 @@
 package net.epoxide.tinker.input;
 
-import com.shc.silenceengine.input.Keyboard;
-import net.epoxide.tinker.entity.living.EntityPlayer;
-
 import java.util.ArrayList;
 import java.util.List;
 
+import com.shc.silenceengine.input.Keyboard;
+
+import net.epoxide.tinker.entity.living.EntityPlayer;
+
 public class KeyHandler {
     public static final List<KeyBind> REGISTRY = new ArrayList<>();
-
+    
     public static final KeyBind forward = new KeyBind(Keyboard.KEY_W);
     public static final KeyBind back = new KeyBind(Keyboard.KEY_S);
     public static final KeyBind left = new KeyBind(Keyboard.KEY_A);
     public static final KeyBind right = new KeyBind(Keyboard.KEY_D);
-
+    
     static {
         REGISTRY.add(forward);
         REGISTRY.add(back);
         REGISTRY.add(left);
         REGISTRY.add(right);
     }
-
+    
     public static void update (EntityPlayer entityPlayer, float delta) {
-
+        
         if (forward.isPressed())
             entityPlayer.setYPos(entityPlayer.getYPos() + delta);
         else if (back.isPressed())
@@ -31,11 +32,11 @@ public class KeyHandler {
             entityPlayer.setXPos(entityPlayer.getXPos() + delta);
         else if (right.isPressed())
             entityPlayer.setXPos(entityPlayer.getXPos() - delta);
-
-        REGISTRY.forEach(keyBind->{
+            
+        REGISTRY.forEach(keyBind -> {
             boolean wasPressed = keyBind.pressed;
             keyBind.setPressed(Keyboard.isPressed(keyBind.getKey()));
-
+            
             if (keyBind.isEvent) {
                 if (!wasPressed && keyBind.isPressed()) {
                     keyBind.onKeyPressed(delta);
