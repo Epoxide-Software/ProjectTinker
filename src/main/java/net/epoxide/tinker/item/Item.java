e3152965e89a27aa5f8314dc3edc94f168e94856
Darkhax <darklime@live.ca>
2016-03-29 04:48:48 -0600

    Items now know their ID. Items can also get their translated name.

--- src/main/java/net/epoxide/tinker/item/Item.java
+++ src/main/java/net/epoxide/tinker/item/Item.java
@@ -5,11 +5,66 @@ import net.epoxide.tinker.entity.EntityStat;
 import net.epoxide.tinker.entity.StatModifier;
 import net.epoxide.tinker.util.ItemObject;
 import net.epoxide.tinker.util.NamedRegistry;
+import net.epoxide.tinker.util.RegistryName;
+import net.epoxide.tinker.util.lang.I18n;
 import net.epoxide.tinker.world.TileMap;
 
 public class Item {
     
     public static final NamedRegistry<Item> REGISTRY = new NamedRegistry<>();
+    
+    /**
+     * The ID that the item is registered under.
+     */
+    public final RegistryName ID;
+    
+    public static final Item ERROR = registerItem(new Item("tinker:error"));
+    
+    /**
+     * Constructs an Item with the ID that the Item was registered under.
+     *
+     * @param id The ID that the Item is registered under.
+     */
+    public Item(String id) {
+        
+        this.ID = new RegistryName(id);
+    }
+    
+    /**
+     * Registers an Item with the {@link #REGISTRY} using the ID stored in the Item. This
+     * should be used over directly accessing the REGISTRY.
+     *
+     * @param item The Item to register.
+     * @return Item The same Item being registered. Provided to make life easier.
+     */
+    public static Item registerItem (Item item) {
+        
+        return REGISTRY.registerValue(item.ID, item);
+    }
+    
+    /**
+     * Gets an Item from the {@link #REGISTRY} which is associated with the name. If no item
+     * exists, {@link #ERROR} will be returned.
+     *
+     * @param name The name of the Item you are looking for.
+     * @return Item The Item associated with the specified name, or {@link #ERROR} if no item
+     *         is found.
+     */
+    public static Item getItemByName (String name) {
+        
+        final Item item = REGISTRY.getValue(name);
+        return (item == null) ? ERROR : item;
+    }
+    
+    /**
+     * Gets the name for the Item translated with the current language.
+     * 
+     * @return String the name for the Item.
+     */
+    public String getTranslatedName () {
+        
+        return I18n.translate("item." + this.ID.getDomain() + "." + this.ID.getName() + ".name");
+    }
     
     /**
      * Gets a StatModifier for the Item. This can allow the Item to manipulate entity stats,
