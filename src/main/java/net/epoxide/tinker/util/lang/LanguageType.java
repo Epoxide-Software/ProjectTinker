b7750503526d4ef2c7b896e3fb1c91524cf57f22
Darkhax <darklime@live.ca>
2016-03-29 04:32:24 -0600

    Added a few additional language types to the list.

--- src/main/java/net/epoxide/tinker/util/lang/LanguageType.java
+++ src/main/java/net/epoxide/tinker/util/lang/LanguageType.java
@@ -2,7 +2,18 @@ package net.epoxide.tinker.util.lang;
 
 public enum LanguageType {
     
-    ENGLISH("en_US"),;
+    ENGLISH("en_US"),
+    CANADIAN("en_CA"),
+    GERMAN("de_DE"),
+    PIRATE("en_PT"),
+    SPANISH_ES("es_ES"),
+    SPANISH_MX("es_MC"),
+    FRENCH("fr_FR"),
+    JAPANESE("ja_JP"),
+    POLISH("pl_PL"),
+    PORTUGUESE("pt_PT"),
+    SWEDISH("sv_SE"),
+    CHINESE("zh_CN");
     
     /**
      * The language identifier. This is used to determine which language is being represented,
