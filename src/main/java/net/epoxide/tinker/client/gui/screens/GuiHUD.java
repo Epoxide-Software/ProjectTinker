e678b3246cfbc4ba6c11829311aa6aca089c9649
Darkhax <darklime@live.ca>
2016-04-02 14:16:34 -0600

    Added @lclc98's missing GuiHUD class.

---
+++ src/main/java/net/epoxide/tinker/client/gui/screens/GuiHUD.java
@@ -0,0 +1,32 @@
+package net.epoxide.tinker.client.gui.screens;
+
+import com.shc.silenceengine.core.Game;
+import com.shc.silenceengine.graphics.Batcher;
+import com.shc.silenceengine.graphics.Color;
+import com.shc.silenceengine.graphics.Graphics2D;
+
+public class GuiHUD extends GuiScreen {
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void update(float delta) {
+    }
+
+    @Override
+    public void drawBackground(Batcher batcher) {
+
+    }
+
+    @Override
+    public void drawForeground(Batcher batcher, float mouseX, float mouseY) {
+
+        super.drawForeground(batcher, mouseX, mouseY);
+        final Graphics2D g2d = Graphics2D.getInstance();
+        g2d.setColor(Color.WHITE);
+        g2d.drawString(String.format("FPS: %s", Game.getFPS()), 0, 0);
+    }
+}
