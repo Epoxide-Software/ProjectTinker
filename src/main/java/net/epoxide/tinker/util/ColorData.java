f3533736297c60f01a7fdc8021c4bc8352e5af18
Darkhax <darklime@live.ca>
2016-04-03 02:34:02 -0600

    Created an enum for silence colors.

---
+++ src/main/java/net/epoxide/tinker/util/ColorData.java
@@ -0,0 +1,176 @@
+package net.epoxide.tinker.util;
+
+import com.shc.silenceengine.graphics.Color;
+
+public enum ColorData {
+    
+    BLACK(0, "0", Color.BLACK),
+    RED(1, "1", Color.RED),
+    GREEN(2, "2", Color.GREEN),
+    BLUE(3, "3", Color.BLUE),
+    WHITE(4, "4", Color.WHITE),
+    ALICE_BLUE(5, "5", Color.ALICE_BLUE),
+    ANTIQUE_WHITE(6, "6", Color.ANTIQUE_WHITE),
+    AQUA(7, "7", Color.AQUA),
+    AQUA_MARINE(8, "8", Color.AQUA_MARINE),
+    AZURE(9, "9", Color.AZURE),
+    BEIGE(10, "a", Color.BEIGE),
+    BISQUE(11, "b", Color.BISQUE),
+    BLANCHED_ALMOND(12, "c", Color.BLANCHED_ALMOND),
+    BLUE_VIOLET(13, "d", Color.BLUE_VIOLET),
+    BROWN(14, "e", Color.BROWN),
+    BURLY_WOOD(15, "f", Color.BURLY_WOOD),
+    CADET_BLUE(16, "10", Color.CADET_BLUE),
+    CHART_REUSE(17, "11", Color.CHART_REUSE),
+    CHOCOLATE(18, "12", Color.CHOCOLATE),
+    CORAL(19, "13", Color.CORAL),
+    CORN_FLOWER_BLUE(20, "14", Color.CORN_FLOWER_BLUE),
+    CORN_SILK(21, "15", Color.CORN_SILK),
+    CRIMSON(22, "16", Color.CRIMSON),
+    CYAN(23, "17", Color.CYAN),
+    DARK_BLUE(24, "18", Color.DARK_BLUE),
+    DARK_CYAN(25, "19", Color.DARK_CYAN),
+    DARK_GOLDEN_ROD(26, "1a", Color.DARK_GOLDEN_ROD),
+    DARK_GRAY(27, "1b", Color.DARK_GRAY),
+    DARK_GREEN(28, "1c", Color.DARK_GREEN),
+    DARK_KHAKI(29, "1d", Color.DARK_KHAKI),
+    DARK_MAGENTA(30, "1e", Color.DARK_MAGENTA),
+    DARK_OLIVE_GREEN(31, "1f", Color.DARK_OLIVE_GREEN),
+    DARK_ORANGE(32, "20", Color.DARK_ORANGE),
+    DARK_ORCHID(33, "21", Color.DARK_ORCHID),
+    DARK_RED(34, "22", Color.DARK_RED),
+    DARK_SALMON(35, "23", Color.DARK_SALMON),
+    DARK_SEA_GREEN(36, "24", Color.DARK_SEA_GREEN),
+    DARK_SLATE_BLUE(37, "25", Color.DARK_SLATE_BLUE),
+    DARK_SLATE_GRAY(38, "26", Color.DARK_SLATE_GRAY),
+    DARK_TURQUOISE(39, "27", Color.DARK_TURQUOISE),
+    DARK_VIOLET(40, "28", Color.DARK_VIOLET),
+    DEEP_PINK(41, "29", Color.DEEP_PINK),
+    DEEP_SKY_BLUE(42, "2a", Color.DEEP_SKY_BLUE),
+    DIM_GRAY(43, "2b", Color.DIM_GRAY),
+    DODGER_BLUE(44, "2c", Color.DODGER_BLUE),
+    FIRE_BRICK(45, "2d", Color.FIRE_BRICK),
+    FLORAL_WHITE(46, "2e", Color.FLORAL_WHITE),
+    FOREST_GREEN(47, "2f", Color.FOREST_GREEN),
+    FUHSIA(48, "30", Color.FUHSIA),
+    GAINSBORO(49, "31", Color.GAINSBORO),
+    GHOST_WHITE(50, "32", Color.GHOST_WHITE),
+    GOLD(51, "33", Color.GOLD),
+    GOLDEN_ROD(52, "34", Color.GOLDEN_ROD),
+    GRAY(53, "35", Color.GRAY),
+    GREEN_YELLOW(54, "36", Color.GREEN_YELLOW),
+    HONEY_DEW(55, "37", Color.HONEY_DEW),
+    HOT_PINK(56, "38", Color.HOT_PINK),
+    INDIAN_RED(57, "39", Color.INDIAN_RED),
+    INDIGO(58, "3a", Color.INDIGO),
+    IVORY(59, "3b", Color.IVORY),
+    KHAKI(60, "3c", Color.KHAKI),
+    LAVENDER(61, "3d", Color.LAVENDER),
+    LAVENDER_BLUSH(62, "3e", Color.LAVENDER_BLUSH),
+    LAWN_GREEN(63, "3f", Color.LAWN_GREEN),
+    LEMON_CHIFFON(64, "40", Color.LEMON_CHIFFON),
+    LIGHT_BLUE(65, "41", Color.LIGHT_BLUE),
+    LIGHT_CORAL(66, "42", Color.LIGHT_CORAL),
+    LIGHT_CYAN(67, "43", Color.LIGHT_CYAN),
+    LIGHT_GOLDEN_ROD(68, "44", Color.LIGHT_GOLDEN_ROD),
+    LIGHT_GRAY(69, "45", Color.LIGHT_GRAY),
+    LIGHT_GREEN(70, "46", Color.LIGHT_GREEN),
+    LIGHT_PINK(71, "47", Color.LIGHT_PINK),
+    LIGHT_SALMON(72, "48", Color.LIGHT_SALMON),
+    LIGHT_SEA_GREEN(73, "49", Color.LIGHT_SEA_GREEN),
+    LIGHT_SKY_BLUE(74, "4a", Color.LIGHT_SKY_BLUE),
+    LIGHT_SLATE_GRAY(75, "4b", Color.LIGHT_SLATE_GRAY),
+    LIGHT_STEEL_BLUE(76, "4c", Color.LIGHT_STEEL_BLUE),
+    LIGHT_YELLOW(77, "4d", Color.LIGHT_YELLOW),
+    LIME(78, "4e", Color.LIME),
+    LIME_GREEN(79, "4f", Color.LIME_GREEN),
+    LINEN(80, "50", Color.LINEN),
+    MAGENTA(81, "51", Color.MAGENTA),
+    MAROON(82, "52", Color.MAROON),
+    MEDIUM_AQUA_MARINE(83, "53", Color.MEDIUM_AQUA_MARINE),
+    MEDIUM_BLUE(84, "54", Color.MEDIUM_BLUE),
+    MEDIUM_ORCHID(85, "55", Color.MEDIUM_ORCHID),
+    MEDIUM_PURPLE(86, "56", Color.MEDIUM_PURPLE),
+    MEDIUM_SEA_GREEN(87, "57", Color.MEDIUM_SEA_GREEN),
+    MEDIUM_SLATE_BLUE(88, "58", Color.MEDIUM_SLATE_BLUE),
+    MEDIUM_SPRING_GREEN(89, "59", Color.MEDIUM_SPRING_GREEN),
+    MEDIUM_TURQUOISE(90, "5a", Color.MEDIUM_TURQUOISE),
+    MEDIUM_VIOLET_RED(91, "5b", Color.MEDIUM_VIOLET_RED),
+    MIDNIGHT_BLUE(92, "5c", Color.MIDNIGHT_BLUE),
+    MINT_CREAM(93, "5d", Color.MINT_CREAM),
+    MISTY_ROSE(94, "5e", Color.MISTY_ROSE),
+    MOCCASIN(95, "5f", Color.MOCCASIN),
+    NOVAJO_WHITE(96, "60", Color.NOVAJO_WHITE),
+    NAVY(97, "61", Color.NAVY),
+    OLD_LACE(98, "62", Color.OLD_LACE),
+    OLIVE(99, "63", Color.OLIVE),
+    OLIVE_DRAB(100, "64", Color.OLIVE_DRAB),
+    ORANGE(101, "65", Color.ORANGE),
+    ORANGE_RED(102, "66", Color.ORANGE_RED),
+    ORCHID(103, "67", Color.ORCHID),
+    PALE_GOLDEN_ROD(104, "68", Color.PALE_GOLDEN_ROD),
+    PALE_GREEN(105, "69", Color.PALE_GREEN),
+    PALE_TURQUOISE(106, "6a", Color.PALE_TURQUOISE),
+    PALE_VIOLET_RED(107, "6b", Color.PALE_VIOLET_RED),
+    PAPAYA_WHIP(108, "6c", Color.PAPAYA_WHIP),
+    PEACH_PUFF(109, "6d", Color.PEACH_PUFF),
+    PERU(110, "6e", Color.PERU),
+    PINK(111, "6f", Color.PINK),
+    PLUM(112, "70", Color.PLUM),
+    POWDER_BLUE(113, "71", Color.POWDER_BLUE),
+    PURPLE(114, "72", Color.PURPLE),
+    ROSY_BROWN(115, "73", Color.ROSY_BROWN),
+    ROYAL_BLUE(116, "74", Color.ROYAL_BLUE),
+    SADDLE_BROWN(117, "75", Color.SADDLE_BROWN),
+    SALMON(118, "76", Color.SALMON),
+    SANDY_BROWN(119, "77", Color.SANDY_BROWN),
+    SEA_GREEN(120, "78", Color.SEA_GREEN),
+    SEA_SHELL(121, "79", Color.SEA_SHELL),
+    SIENNA(122, "7a", Color.SIENNA),
+    SILVER(123, "7b", Color.SILVER),
+    SKY_BLUE(124, "7c", Color.SKY_BLUE),
+    SLATE_BLUE(125, "7d", Color.SLATE_BLUE),
+    SLATE_GRAY(126, "7e", Color.SLATE_GRAY),
+    SNOW(127, "7f", Color.SNOW),
+    SPRING_GREEN(128, "80", Color.SPRING_GREEN),
+    STEEL_BLUE(129, "81", Color.STEEL_BLUE),
+    TAN(130, "82", Color.TAN),
+    TEAL(131, "83", Color.TEAL),
+    THISTLE(132, "84", Color.THISTLE),
+    TOMATO(133, "85", Color.TOMATO),
+    TURQUOISE(134, "86", Color.TURQUOISE),
+    VIOLET(135, "87", Color.VIOLET),
+    WHEAT(136, "88", Color.WHEAT),
+    WHITE_SMOKE(137, "89", Color.WHITE_SMOKE),
+    YELLOW(138, "8a", Color.YELLOW),
+    YELLOW_GREEN(139, "8b", Color.YELLOW_GREEN);
+    
+    /**
+     * The index of the color in the Enum. This is 0 indexed.
+     */
+    public final int index;
+    
+    /**
+     * A hex representation of the index.
+     */
+    public final String hex;
+    
+    /**
+     * The Color being represented.
+     */
+    public final Color color;
+    
+    /**
+     * Constructs a new ColorData enum object.
+     * 
+     * @param index The index of the color in the Enum.
+     * @param hex The hex representation of the index.
+     * @param color The color being represented.
+     */
+    ColorData(int index, String hex, Color color) {
+        
+        this.index = index;
+        this.hex = hex;
+        this.color = color;
+    }
+}
